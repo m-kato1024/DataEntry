04427c475427ad2f3a184e60e2c601695d4eecc4
takanori jogasaki <takanori_jogasak@solver.co.jp>
2020-08-07 17:01:41 +0900

    UIのメインメニュー表示を追加

---
+++ DataEntry/Message.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define MSG_MAIN_MENU_TITLE		"[メインメニュー]"
+#define MSG_MAIN_MENU_ADDNEW	"新規登録"
+#define MSG_MAIN_MENU_LIST		"一覧表示"
+#define MSG_MAIN_MENU_SEARCH	"読み仮名検索"
+#define MSG_MAIN_MENU_END		"終了"
+
--- DataEntry/UserInterface.c
+++ DataEntry/UserInterface.c
@@ -1,3 +1,9 @@
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+
+void UIAddnew() {
+	//V‹K“o˜^ˆ—
+}
 
