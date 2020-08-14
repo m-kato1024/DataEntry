8cab98081f08c2fb6e657aea4f6a8db83bcd3c05
ssc-kinoshita <takuma_kinoshita@solver.co.jp>
2020-08-14 09:34:35 +0900

    実装指摘レビュー437　ヘッダーファイルにコメント追加

--- DataEntry/DataManager.h
+++ DataEntry/DataManager.h
@@ -1,14 +1,26 @@
 #pragma once
+#include <stdbool.h>
+
+//--------------------------------------------------------------------
+//define定義
+//--------------------------------------------------------------------
 //データ登録の最大件数
 #define DATA_MAX_COUNT (10)
 //名前の最大バイト数
 #define DATA_MAX_LENGTH (40)
-#include <stdbool.h>
+
+//--------------------------------------------------------------------
+//型定義
+//--------------------------------------------------------------------
 struct data {
 	int number;
 	char name[DATA_MAX_LENGTH];
 	char yomi[DATA_MAX_LENGTH];
 };
+
+//--------------------------------------------------------------------
+//関数宣言
+//--------------------------------------------------------------------
 bool DMInitialization(char* path);
 bool DMAddNew(int input_number, char* input_name, char* input_yomi);
 void DMDelete(int input_number);
